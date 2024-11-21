d2ba0cada51452748da72576ffc8a57c548ae2d6
Abubakar Kabiru <abubakarringim@gmail.com>
2024-11-21 10:48:07 +0100

    Payment Screen

---
+++ src/components/PaymentMethod.tsx
@@ -0,0 +1,106 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import LinearGradient from 'react-native-linear-gradient';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+import CustomIcon from './CustomIcon';
+
+interface PaymentMethod{
+  paymentMode: string;
+  name:string ;
+  icon:any;
+  isIcon: boolean;
+}
+
+const PaymentMethod: React.FC<PaymentMethod> = (
+  {
+  paymentMode,
+  name,
+  icon,
+  isIcon,
+  }
+) => {
+  return (
+    <View style={[styles.PaymentCardContainer, {
+      borderColor: paymentMode == name ? COLORS.primaryOrangeHex : COLORS.primaryGreyHex
+    }]}>
+      {isIcon ? (
+        <LinearGradient 
+        start={{x:0, y:0}} 
+        end={{x:1, y:1}}
+        colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]} 
+        style={styles.LinearGradientWallet}>
+          <View style={styles.WalletRow}>
+            <CustomIcon 
+            name={'wallet'}
+            color={COLORS.primaryOrangeHex}
+            size={FONTSIZE.size_30} />
+            <Text style={styles.PaymentTitle}>{name}</Text>
+            
+          </View>
+          <Text style={styles.PaymentPrice}>$ 100.50</Text>
+        </LinearGradient>
+      ) : ( 
+        <LinearGradient 
+        start={{x:0, y:0}} 
+        end={{x:1, y:1}}
+        colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]} 
+        style={styles.LinearGradientRegular}>
+         
+         <Image source={icon} style={styles.PaymentImage}/>
+          <Text style={styles.PaymentTitle}> {name} </Text>
+
+        </LinearGradient>
+      )
+    }
+    </View> 
+  )
+}
+
+
+const styles = StyleSheet.create({
+  PaymentCardContainer:{
+    borderRadius: BORDERRADIUS.radius_15 * 2,
+    backgroundColor: COLORS.primaryWhiteHex,
+    borderWidth: 3,
+  },
+
+  LinearGradientWallet:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: SPACING.space_12,
+    gap: SPACING.space_24,
+    borderRadius: BORDERRADIUS.radius_15 * 2,
+
+  },
+  WalletRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: SPACING.space_24,
+
+  },
+  PaymentPrice :{
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_16,
+    color: COLORS.secondaryLightGreyHex,
+  },
+  PaymentTitle:{
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_16,
+    color: COLORS.primaryWhiteHex,
+  },
+  LinearGradientRegular :{
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: SPACING.space_12,
+    gap: SPACING.space_24,
+    borderRadius: BORDERRADIUS.radius_15 * 2,
+
+  },
+  PaymentImage :{
+    height: SPACING.space_32,
+    width: SPACING.space_32,
+  }
+})
+
+export default PaymentMethod
